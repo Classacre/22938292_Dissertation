16325b88e425f112f9a03be17f5dbe73f9c03d4d
Classacre <nieuwenhuyzenmikamartin@gmail.com>
2025-06-25 14:24:09 +0800

    Removing of test files nore expression_matrix.csv

--- test/replace_seurat_path.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script to replace all references to the old Seurat object with the new col-only version
-# Searches all files in /group/sms029/mnieuwenh recursively
-
-find /group/sms029/mnieuwenh -type f -exec sed -i 's|/group/sms029/Oliva_dataset/integrated_col_trajectories_colonly.rds|/group/sms029/Oliva_dataset/integrated_col_trajectories_colonly.rds|g' {} +
-
-echo "All references updated to use integrated_col_trajectories_colonly.rds."
